3d3b6a248e747826f80a02cf6d0550d29fa2aa15
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-03-11 23:02:26 +0100

    adding the logic for the TODO CLI

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,93 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	todo "github.com/ramzibouzaiene/Go-Lang-CLI-APP"
+)
+
+const (
+	todoFile = ".todos.json"
 )
 
 func main() {
-	fmt.Println("hello")
+	add := flag.Bool("add", false, "Add a new todo")
+	complete := flag.Int("complete", 0, "Mark a todo as completed")
+	delete := flag.Int("delete", 0, "Delete a Todo")
+	list := flag.Bool("list", false, "List All Todos")
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		todos.Add(task)
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *delete > 0:
+		err := todos.Delete(*delete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
+	default:
+		fmt.Fprintln(os.Stdout, "Invalid Command")
+		os.Exit(0)
+	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, ""), nil
+	}
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", nil
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
